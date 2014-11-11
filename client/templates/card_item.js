b1ecd494dc8cf1f3584c4df9db53863b40fa813f
Joe Shepherd <joeshepmedia@gmail.com>
2014-11-11 11:15:29 -0600

    Add counter; fix checkMatch bug

--- client/templates/card_item.js
+++ client/templates/card_item.js
@@ -1,3 +1,6 @@
+var counter = 0;
+var matches = 0;
+
 Template.cardItem.helpers({
   domain: function() {
     var pic = document.createElement('img');
@@ -17,10 +20,11 @@ Template.cardItem.events({
         speed: 200,
         color: 'white',
         onEnd: function(){
+          // debugger;
           if($('.flipped:eq(1)').length){
             checkMatch();
-            // counter += 1;
-            // $('#flip-count').text(' ' + counter);
+            counter += 1;
+            $('#flip-count').text(' ' + counter);
           }
         }
       }).addClass('flipped');
@@ -33,25 +37,37 @@ Template.cardItem.events({
 });
 
 function checkMatch(){
-    var $pic1 = $('.flipped:eq(0)').find('img').data('id');
-    console.log($pic1);
-    var $pic2 = $('.flipped:eq(1)').find('img').data('id');
-    if( $pic1 === $pic2 ){
-      $('.flipped').addClass('matched').removeClass('flipped');
-      $('.matched').children().css('outline','5px solid green');
-      // playMultiSound($('audio[id=a'+$pic1+']'));
-      // $('#boardContainer').animate({'background-color':'green'},300).delay(300).animate({'background-color':'#7792A8'},200);
-      // matches += 1;
-      // $('#match-count').text(' ' + matches);
-      if($('.matched').length === 20){
-        setTimeout(function(){
-          alert('Great job!');
-        },500);
-      }
-    } else {
-      // playMultiSound($('#wrong'));
+  var $pic1 = $('.flipped:eq(0)').find('img').data('id');
+  var $pic2 = $('.flipped:eq(1)').find('img').data('id');
+
+  if( $pic1 === $pic2 ){
+    $('.flipped').addClass('matched').removeClass('flipped');
+    $('.matched').children().css('outline','5px solid green');
+    $('#ding')[0].play();
+    playMultiSound($('audio[class="'+$pic1+'"]'));
+    $('#js-boardContainer').animate({'background-color':'green'},300).delay(300).animate({'background-color':'#7792A8'},200);
+    matches += 1;
+    $('#match-count').text(' ' + matches);
+
+    if($('.matched').length === 20){
+      setTimeout(function(){
+        alert('Great job!');
+      },500);
+    }
+  } else {
+      $('#wrong')[0].play();
       setTimeout(function(){
         $('.flipped').revertFlip().removeClass('flipped');
       },800);
     }
-  }
+}
+
+function playMultiSound(s) {
+  var soundFX = new Audio();
+  soundFX.src = s.attr('src');
+  soundFX.volume = s.attr('volume') || "1.0";
+  soundFX.load();
+  soundFX.play();
+  // debugger;
+}
+
